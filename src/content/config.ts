13ac85f2b40df48cf025ff379882ae380f0615b0
Chuyang Ke <11568203+ChuyangKe@users.noreply.github.com>
2026-04-18 21:33:42 -0700

    Revert "Update astro and refactor blog content handling"
    This reverts commit 66957b8e15e53629615c24eb9dbade65602a0521.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-	loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
+	type: 'content',
+	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		tags: z.array(z.string()).default([]),
+        tags: z.array(z.string()).default([]),
 		draft: z.boolean().default(false),
 	}),
 });
